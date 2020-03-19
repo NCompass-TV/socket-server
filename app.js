037466d2e3efb76efbdbe7c34991c11ac475ce09
NCompass-TV <ncompass.git@gmail.com>
2020-03-20 05:43:42 +0800

    socket server test

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.get('/ping', (req, res) => res.send('pong'));
 
 // 2. Start Server
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server started on port ${PORT}`);
 });
 
@@ -24,6 +24,7 @@ io.sockets.on('connection', (socket) => {
     // 5. Listen to update_pi event
     socket.on('update_pi', (data) => {
         console.log(`Update Received from: ${socket.id}; Data: ${data};`);
+        io.sockets.emit('launch_update', data);
     })
 
     socket.on('disconnect', () => {
