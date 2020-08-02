16c79cf5e955bbae474aeb6c8d4562cd756fbffa
earlvhin <earlgabuat@gmail.com>
2020-08-03 01:29:25 +0800

    Set to API STAGING

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const socket = require('socket.io');
 const PORT = process.env.PORT;
-const API_DEV = process.env.API_STAGING;
+const API_STAGING = process.env.API_STAGING;
 const app = express();
 const axios = require('axios');
 let license_socket = [];
@@ -262,7 +262,7 @@ io.sockets.on('connection', (socket) => {
 })
 
 const appendSocketToLicense = (pi_data) => {
-    axios.post(`${API_DEV}/license/UpdateSocketIds`, pi_data)
+    axios.post(`${API_STAGING}/license/UpdateSocketIds`, pi_data)
     .then((res) => {    
         console.log('License Socket Updated: ', pi_data);
     }).catch((error) => {
@@ -271,7 +271,7 @@ const appendSocketToLicense = (pi_data) => {
 }
 
 const getLicenseSocketID = license_id => {
-    return axios.get(`${API_DEV}/license/GetSocketByLicense?licenseId=${license_id}`)
+    return axios.get(`${API_STAGING}/license/GetSocketByLicense?licenseId=${license_id}`)
     .then((res) => {
         return res.data;
     }).catch((error) => {
@@ -280,7 +280,7 @@ const getLicenseSocketID = license_id => {
 }
 
 const getSocketLicenseId = socket_id => {
-    return axios.get(`${API_DEV}/License/GetByPiSocketId?socketid=${socket_id}`)
+    return axios.get(`${API_STAGING}/License/GetByPiSocketId?socketid=${socket_id}`)
     .then(function (response) {
         return response.data
     }).catch(function (error) {
@@ -289,7 +289,7 @@ const getSocketLicenseId = socket_id => {
 }
 
 const offlineNotification = (data) => {
-    axios.post(`${API_DEV}/notification/send`, data)
+    axios.post(`${API_STAGING}/notification/send`, data)
     .then(function (response) {
         console.log('Disconnected signal sent successfully', data);
     }).catch(function (error) {
@@ -298,7 +298,7 @@ const offlineNotification = (data) => {
 }
 
 const updateLicensePiandPlayerStatus = (data) => {
-    axios.post(`${API_DEV}/license/UpdatePiPlayerStatus`, data)
+    axios.post(`${API_STAGING}/license/UpdatePiPlayerStatus`, data)
     .then(res => {
         console.log('Status successfully updated', res.status);
     }).catch(err => {
