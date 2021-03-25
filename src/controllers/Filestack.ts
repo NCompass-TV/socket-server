77c3f85a0fd47422adfd0228aa6448b5b60b79f1
earlvhin <earlgabuat@gmail.com>
2021-03-25 23:40:22 +0800

    fix(filestack.ts): filestack route socket error
    filestack converted video route, socket emit error

--- src/controllers/Filestack.ts
+++ src/controllers/Filestack.ts
@@ -5,18 +5,19 @@ import { ActivityLogger } from '../helpers/AcitivityLogger';
 import { FILESTACK } from '../constants/SocketEvents';
 import { LOG_TYPES } from '../constants/Logger';
 import { Router, Request, Response } from 'express';
+import { Server } from 'socket.io';
 import { SetConvertedVideoStatus } from '../helpers/SetConvertedVideoStatus';
 
 const filestackRoutes: Router = Router();
 
 filestackRoutes.post('', async (req: Request, res: Response) => {
 	try {
-		const io = req.app.get('io');
+		const io: Server = req.app.get('io');
 		await new SetConvertedVideoStatus().invoke(req.body.uuid);
-		io.sockets.emit(FILESTACK.video_converted, req.body.uuid);
+		io.emit(FILESTACK.video_converted, req.body.uuid);
 	} catch(error) {
 		new ActivityLogger(LOG_TYPES.error,
-            `Error on #filestackRoutes_post: { message: ${error.message}}`);
+            `Error on #filestackRoutes_post: ${error.message}`);
 	}
 })
 
